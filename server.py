80b6267a269fff043221300ccef586a24e7ce591
Glitch (dpower-calc) <none>
2020-02-12 14:28:20 +0000

    🐃🙌 Updated with Glitch

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello World!"
+
+@app.route("/upload")
+def uploadCSV(self):
+    file = request.files['file']
+    data = pd.read_csv(file)
+    print(data)
 
 if __name__ == "__main__":
   app.run()
