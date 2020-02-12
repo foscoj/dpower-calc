d3baa56bf3a84030f992181dc0b44b501e0d5f51
Jonas <jodorn@gmail.com>
2020-02-12 14:02:42 +0000

    added glitch files

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        df = pd.read_csv('data\\Day-ahead Prices_201901010000-202001010000.csv',skiprows=1,names=['mtu','price_mwh'])
+        df = pd.read_csv('../data/csv/Day-ahead Prices_201901010000-202001010000.csv',skiprows=1,names=['mtu','price_mwh'])
         df['price_kwh'] = df['price_mwh'] / 1000
         df = df.drop(['price_mwh'], axis=1)
         new = df['mtu'].str.split(' - ',n=1,expand=True)
